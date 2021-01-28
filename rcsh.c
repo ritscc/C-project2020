29f11e699f942eed3e84e2ad0b3a36c1c6a3b3ea
3to5thpower <nisi3genki@gmail.com>
2021-01-28 23:21:10 +0900

    :recycle: 関数に分割したら最適化オプションをつけないと動かないコードができた

--- rcsh.c
+++ rcsh.c
@@ -1,65 +1,80 @@
 #include <error.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define BUFSIZE 1024
-#define DEFAULT_MAX_ARGC 10
-#define TOK_DELIM " \t\r\n\a"
+int read_cmd(char ***);
+pid_t execute(char *argv[]);
 
 int main(void) {
-    char input[BUFSIZE];
+    char **argv;
 
     while (1) {
-        // prompt & input
         printf("rcsh> ");
-        fgets(input, BUFSIZE, stdin);
-        input[strlen(input) - 1] = '\0';
-
-        //引数の個数とコマンド保存用文字列配列の宣言
-        int argc = 0, max_argc = DEFAULT_MAX_ARGC;
-        char **argv = calloc(DEFAULT_MAX_ARGC, sizeof(char *));
-
-        // strtokは1回目の呼び出しではTOK_DELIMを一切含まない最初の部分をinputから切り出す
-        //この際word直後のTOK_DELIM文字は\0に置き換えられる
-        char *word = strtok(input, TOK_DELIM);
-
-        while (word != NULL) {
-            argv[argc++] = word;
-
-            //引数の個数が配列の要素数より多かった時の対処
-            if (argc >= max_argc) {
-                max_argc *= 2;
-
-                // reallocは第1引数の内容をコピーした新しい領域を返す.
-                argv = realloc(argv, max_argc * sizeof(char *));
-                if (argv == NULL) {
-                    fprintf(stderr, "rcsh: allocation error\n");
-                    exit(1);
-                }
-            }
-
-            // 2回目以降はstrtokが1回目に呼び出した文字列を保持しているためNULLを与える
-            word = strtok(NULL, TOK_DELIM);
-        }
-
-        if (argc < 1) continue;
-        argv[argc] = NULL;
-
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp(argv[0], argv);
-            perror("child process error:");
-            exit(1);
-        }
-
+        if (read_cmd(&argv) < 1) continue;
+        pid_t pid = execute(argv);
         int status;
         wait(&status);
-        printf("Child process exited with status %d\n", WEXITSTATUS(status));
-
+        printf("Exited with status %d\n", WEXITSTATUS(status));
         free(argv);
     }
     return 0;
+}
+
+int read_cmd(char ***argv) {
+    const int BUFSIZE = 1024;
+    const char TOK_DELIM[] = " \t\r\n\a";
+
+    int argc = 0, max_argc = 10;
+    char input[BUFSIZE];
+    *argv = (char **)calloc(max_argc, sizeof(char *));
+
+    // input
+    fgets(input, BUFSIZE, stdin);
+    if (feof(stdin)) {
+        return 0;
+    }
+    input[strlen(input) - 1] = '\0';
+
+    // strtokは2回目の呼び出しではTOK_DELIMを一切含まない最初の部分をinputから切り出す
+    //この際word直後のTOK_DELIM文字は\0に置き換えられる
+    char *word = strtok(input, TOK_DELIM);
+    (*argv)[argc++] = word;
+
+    while ((word = strtok(NULL, TOK_DELIM))) {
+        (*argv)[argc++] = word;
+
+        //引数の個数が配列の要素数より多かった時の対処
+        if (argc >= max_argc) {
+            max_argc *= 2;
+
+            // reallocは第1引数の内容をコピーした新しい領域を返す.
+            argv = realloc(argv, max_argc * sizeof(char *));
+            if (argv == NULL) {
+                fprintf(stderr, "rcsh: allocation error\n");
+                exit(1);
+            }
+        }
+    }
+
+    if ((*argv)[argc] != NULL) {
+        argv = realloc(argv, (argc + 2) * sizeof(char *));
+        (*argv)[argc] = NULL;
+    }
+
+    return argc;
+}
+
+// gcc -O2でコンパイルするとなぜか動くがフラグがないとargvが壊れる
+pid_t execute(char *argv[]) {
+    pid_t pid;
+    pid = fork();
+    if (pid != 0) return pid;
+
+    execvp(argv[0], argv);
+    perror("Child Process Error");
+    exit(1);
 }
