f76166d392944af81469e82e8d7477c155a6a95b
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-02 21:34:27 +0900

    feat: 挿入なしの場合の差分を表示

--- commands/diff/diff.c
+++ commands/diff/diff.c
@@ -2,6 +2,27 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define MAX_BUF (256)
+
 int main(int argc,char *argv[]){
+    FILE *file[2];
+    char str1[MAX_BUF];
+    char str2[MAX_BUF];
+
+    file[0] = fopen(argv[1], "r");
+    file[1] = fopen(argv[2], "r");
+    if (file[0] == NULL||file[1] == NULL) {
+        printf("no exist\n\n");
+    }
+    while ((fgets(str1, MAX_BUF - 1, file[0])) != NULL) {
+        fgets(str2, MAX_BUF - 1, file[1]);
+        if(strcmp(str1,str2)){
+            printf("%s",str2);
+        }
+    }
+    printf("\n\n");
+    fclose(file[0]);
+    fclose(file[1]);
+
     return 0;
 }
