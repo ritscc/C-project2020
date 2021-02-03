51a9dbafa2dab3471b0ca87c586fee6c441db7f0
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-02-03 15:38:56 +0900

    feat: ファイルの内容を一旦文字列配列に格納してから比較するようにした

--- commands/diff/diff.c
+++ commands/diff/diff.c
@@ -6,8 +6,8 @@
 
 int main(int argc,char *argv[]){
     FILE *file[2];
-    char str1[MAX_BUF];
-    char str2[MAX_BUF];
+    char str1[MAX_BUF][MAX_BUF];
+    char str2[MAX_BUF][MAX_BUF];
 
     if(argc!=3){
         printf("usage:diff file1 file2\n");
@@ -19,10 +19,17 @@ int main(int argc,char *argv[]){
     if (file[0] == NULL||file[1] == NULL) {
         printf("no exist\n\n");
     }
-    while ((fgets(str1, MAX_BUF - 1, file[0])) != NULL) {
-        fgets(str2, MAX_BUF - 1, file[1]);
-        if(strcmp(str1,str2)){
-            printf("%s",str2);
+    int num1=0,num2=0;
+    while ((fgets(str1[num1], MAX_BUF - 1, file[0])) != NULL) {
+        num1++;
+    }
+    while ((fgets(str2[num2], MAX_BUF - 1, file[1])) != NULL) {
+        num2++;
+    }
+    for (int i=0;i<num1;i++) {
+        if(strcmp(str1[i],str2[i])){
+            printf("%d\n",i+1);
+            printf("%s",str2[i]);
         }
     }
     printf("\n\n");
