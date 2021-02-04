142ed2869d14e30a956c9f152e67b2310af355fa
3to5thpower <nisi3genki@gmail.com>
2021-02-04 19:57:31 +0900

    :sparkles: bg実行機能を実装

--- rcsh.c
+++ rcsh.c
@@ -1,37 +1,54 @@
+#include <ctype.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-char** read_cmd(int*);
+char **read_cmd(int *, bool *);
 pid_t execute(char *argv[]);
+void sigchld_wait(int sig);
 
 int main(void) {
+    if (signal(SIGCHLD, sigchld_wait) == SIG_ERR) {
+        fprintf(stderr, "rcsh: signal(SIGCHLD) failed\n");
+        exit(1);
+    }
+
     while (1) {
         printf("rcsh> ");
-        int argc;
-        char** argv = read_cmd(&argc);
-        if(argc < 1) continue;
+        int argc = 0;
+        bool exec_bg = false;
+        char **argv = read_cmd(&argc, &exec_bg);
+        if (argc < 1) {
+            free(*argv);
+            free(argv);
+            continue;
+        }
 
-        if(strcmp(argv[0], "exit") == 0) break;
+        if (strcmp(argv[0], "exit") == 0) break;
+
         pid_t pid = execute(argv);
         int status;
-        wait(&status);
-        printf("Exited with status %d\n", WEXITSTATUS(status));
+        if (!exec_bg) {
+            wait(&status);
+            printf("Exited with status %d\n", WEXITSTATUS(status));
+        }
+        free(*argv);
         free(argv);
     }
     return 0;
 }
 
-char** read_cmd(int* argc) {
+char **read_cmd(int *argc, bool *exec_bg) {
     const int BUFSIZE = 1024;
     const char TOK_DELIM[] = " \t\r\n\a";
 
-    
     int arg_cnt = 0, max_argc = 10;
-    //char input[BUFSIZE];
-    char *input = malloc(sizeof(char)*BUFSIZE);
+    // char input[BUFSIZE];
+    char *input = malloc(sizeof(char) * BUFSIZE);
     char **argv = malloc(max_argc * sizeof(char *));
 
     // input
@@ -40,6 +57,14 @@ char** read_cmd(int* argc) {
         return 0;
     }
     input[strlen(input) - 1] = '\0';
+
+    // バックグラウンド実行のフラグを設定
+    // 同時に引数から&記号を削除
+    char *pos_and;
+    if ((pos_and = strchr(input, '&')) != NULL) {
+        *exec_bg = true;
+        *pos_and = '\0';
+    }
 
     // strtokは2回目の呼び出しではTOK_DELIMを一切含まない最初の部分をinputから切り出す
     //この際word直後のTOK_DELIM文字は\0に置き換えられる
@@ -63,15 +88,18 @@ char** read_cmd(int* argc) {
     }
 
     if (argv[arg_cnt] != NULL) {
-        argv = realloc((char**)*argv, (arg_cnt + 2) * sizeof(char *));
+        argv = realloc((char **)*argv, (arg_cnt + 2) * sizeof(char *));
         argv[arg_cnt] = NULL;
     }
 
+    if (argv[0] == NULL) {
+        arg_cnt--;
+    }
     *argc = arg_cnt;
+
     return argv;
 }
 
-// gcc -O2でコンパイルするとなぜか動くがフラグがないとargvが壊れる
 pid_t execute(char *argv[]) {
     pid_t pid = fork();
     if (pid != 0) return pid;
@@ -81,3 +109,8 @@ pid_t execute(char *argv[]) {
     exit(1);
 }
 
+void sigchld_wait(int sig) {
+    int status;
+    waitpid(-1, &status, WNOHANG);
+    printf("process exited with status %d\n", WEXITSTATUS(status));
+}
