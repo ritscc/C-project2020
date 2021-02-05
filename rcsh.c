d5922512bafae2ea943b4173f0556f959657e1e0
3^5 <39331917+3to5thpower@users.noreply.github.com>
2021-02-05 14:41:45 +0900

    shell実装 (#8)
    * :sparkles: implement fork exec routine
    
    * :construction_worker: add makefile
    
    * :bug: update makefile
    
    * :sparkles: implement read fork exec loop
    
    * :green_heart: enable debug build
    
    * :green_heart: add make clean
    
    * :green_heart: add warning flag
    
    * :recycle: 関数に分割したら最適化オプションをつけないと動かないコードができた
    
    * :sparkles: 解決
    
    * :recycle: ゴミファイル削除
    
    * Delete .idea directory
    
    * :sparkles: bg実行機能を実装
    
    * :sparkles: [pwd] サンプルのpwdを実装
    
    * :construction_worker: add pwd makefile
    
    * :see_no_evil: add pwd .gitignore
    
    * :sparkles: 組み込みコマンド検索を実装
    
    * :sparkles: 組み込みコマンド実行機能を実装
    
    * :green_heart: fix pwd Makefile
    
    * :construction_worker: update Makefile for building internal commands
    
    * :recycle: fix duplicate tab
    
    * :recycle: ファイル分割
    
    * :bug: ^DでのSEGVを解決
    
    * :sparkles: 親プロセス側は^Cを無視するように設定
    
    * :recycle: rename function get_internal_cmd to get_inner_cmd

---
+++ rcsh.c
@@ -0,0 +1,86 @@
+#include <ctype.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "inner_cmds.c"
+#include "read_cmd.c"
+
+pid_t execute(char *, char **);
+void sigchld_wait(int);
+void sigint_ignore(int sig) { return; }
+
+int main(void) {
+    // bg実行終了後のリソース回収処理
+    if (signal(SIGCHLD, sigchld_wait) == SIG_ERR) {
+        fprintf(stderr, "rcsh: signal(SIGCHLD) failed\n");
+        exit(1);
+    }
+
+    int argc = 0, status;
+    bool exec_bg = false;
+    char **argv;
+    pid_t pid;
+
+    // 組み込みコマンドの取得
+    int inner_cmds_cnt;
+    char **inner_cmds = get_inner_cmd(&inner_cmds_cnt);
+
+    while (1) {
+        // ^Cを無視
+        if (signal(SIGINT, SIG_IGN) == SIG_ERR) {
+            fprintf(stderr, "rcsh: signal(SIGINT) failed\n");
+            exit(1);
+        }
+
+        // read
+        if ((argv = read_cmd(&argc, &exec_bg)) == NULL) {
+            break;
+        }
+
+        // no input
+        if (argc < 1) {
+            free_argv(argv);
+            continue;
+        }
+
+        // exit
+        if (strcmp(argv[0], "exit") == 0) {
+            free_argv(argv);
+            break;
+        }
+
+        // update cmdpath if internal command
+        char *cmd = check_inner_cmd(inner_cmds_cnt, inner_cmds, argv);
+
+        // execute
+        pid = execute(cmd, argv);
+
+        // wait
+        if (!exec_bg) {
+            wait(&status);
+        }
+
+        free_argv(argv);
+    }
+
+    free_inner_cmds(inner_cmds_cnt, inner_cmds);
+    return 0;
+}
+
+pid_t execute(char *cmd, char *argv[]) {
+    pid_t pid = fork();
+    if (pid != 0) return pid;
+
+    signal(SIGINT, SIG_DFL);
+    execvp(cmd, argv);
+    perror(argv[0]);
+    exit(1);
+}
+
+void sigchld_wait(int sig) {
+    int status;
+    waitpid(-1, &status, WNOHANG);
+    printf("process exited with status %d\n", WEXITSTATUS(status));
+}
