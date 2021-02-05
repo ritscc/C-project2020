4bb23253f2e033412c52f92d587b82f4d1b40240
Tsubokura <nekodaisuki169@gmail.com>
2021-02-05 15:45:52 +0900

    make .gitignore Makefile and fix mv.c

--- commands/mv.c
+++ commands/mv.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
     int ret;
     struct stat buf;
 
-    if (argc != 3){
+    if(argc != 3){
       printf("argument shape is not correct!\n");
       exit(1);
     }
@@ -18,35 +18,34 @@ int main(int argc, char** argv){
       printf("%s is not exist\n", argv[1]);
       exit(1);
     }
-
     //if argv[2] is directory
     ret = stat(argv[2], &buf);
     if(S_ISDIR(buf.st_mode)){
-      puts("aaaa");
+    
       char newfilename[256];
       char srash[] = "/";
 
-      //x86_64 strlcpyなどはアーキテクチャのみで動く？
+      // strlcpyなどはx86_64アーキテクチャのみで動く？
       strlcpy(newfilename, argv[2], sizeof(newfilename));
       strlcat(newfilename, srash, sizeof(newfilename));
       strlcat(newfilename, argv[1], sizeof(newfilename));
 
-      if (rename(argv[1], newfilename) == 0){
+      if(rename(argv[1], newfilename) == 0){
         printf("%s move to %s\n", argv[1], newfilename);
         exit(0);
       }
-      else {
+      else{
         printf("destination directory is not exist,move is not possible\n");
         exit(1);
       }
     }
 
     //if argv[2] is filename
-    if (rename(argv[1], argv[2]) == 0){
+    if(rename(argv[1], argv[2]) == 0){
       printf("%s move to %s\n", argv[1], argv[2]);
       exit(0);
     }
-    else {
+    else{
       printf("destination name is not exist, so move is not possible\n");
       exit(1);
     }
